706aa3636c90591ee5413711c201c819dac91be6
nail-brk <briknail@gmail.com>
2025-07-31 19:37:05 +0200

    Refactorisation de nbmystere

--- nbmyst.cpp
+++ nbmyst.cpp
@@ -1,6 +1,43 @@
-char rejouer;
-do {
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+
+int aleatoire(int max) {
+    return rand() % (max + 1);
+}
+
+
+void nbmyst() {
+    int maxVal;
+    int nbEssais = 0;
+    int proposition;
+    
+    cout << "Choisissez la valeur maximale pour le nombre mystère : ";
+    cin >> maxVal;
+
+    int secret = aleatoire(maxVal);
+
+    do {
+        cout << "Entrez une valeur : ";
+        cin >> proposition;
+        nbEssais++;
+
+        if (proposition < secret) {
+            cout << "C'est plus !" << endl;
+        } else if (proposition > secret) {
+            cout << "C'est moins !" << endl;
+        } else {
+            cout << "Bravo ! Vous avez trouvé la valeur en " << nbEssais << " essai";
+            if (nbEssais > 1) cout << "s";
+            cout << " !" << endl;
+        }
+
+    } while (proposition != secret);
+}
+
+int main() {
+    srand(time(NULL)); 
     nbmyst();
-    cout << "Voulez-vous rejouer ? (o/n) : ";
-    cin >> rejouer;
-} while (rejouer == 'o' || rejouer == 'O');
+}
