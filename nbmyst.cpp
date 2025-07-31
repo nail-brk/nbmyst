9337ede4c1de35430d17765fe77dcdd7ba834591
nail-brk <briknail@gmail.com>
2025-07-31 19:51:13 +0200

    Fonction principale

--- nbmyst.cpp
+++ nbmyst.cpp
@@ -1,26 +1,5 @@
-void nbmyst() {
-    int maxVal;
-    int nbEssais = 0;
-    int proposition;
-
-    cout << "Choisissez la valeur maximale pour le nombre mystère : ";
-    cin >> maxVal;
-
-    int secret = aleatoire(maxVal);
-
-    do {
-        cout << "Entrez une valeur : ";
-        cin >> proposition;
-        nbEssais++;
-
-        if (proposition < secret) {
-            cout << "C'est plus !" << endl;
-        } else if (proposition > secret) {
-            cout << "C'est moins !" << endl;
-        } else {
-            cout << "Bravo ! Vous avez trouvé la valeur en " << nbEssais
-                 << " essai" << (nbEssais > 1 ? "s" : "") << " !" << endl;
-        }
-
-    } while (proposition != secret);
+int main() {
+    srand(time(NULL)); // initialise le générateur aléatoire
+    nbmyst();
+    return 0;
 }
